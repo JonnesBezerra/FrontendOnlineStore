2aec97d406c1996f34daf2ef1d7b21be1d0423f2
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-09-26 23:53:58 -0300

    Fix: log error

--- src/Components/CartButton.jsx
+++ src/Components/CartButton.jsx
@@ -4,21 +4,20 @@ import { Link } from 'react-router-dom';
 class CartButton extends Component {
   render() {
     return (
-      <Link
-        to="/shopping-cart"
-        data-testid="shopping-cart-button"
-      >
-        <ul>
-          <li>
-            <a href="/" className="cart position-relative" aria-label="shopping cart">
-              <i className="fas fa fa-shopping-cart fa-lg" />
-              <span className="cart-basket d-flex justify-content-center">
-                0
-              </span>
-            </a>
-          </li>
-        </ul>
-      </Link>
+      <ul>
+        <li>
+          <Link
+            to="/shopping-cart"
+            data-testid="shopping-cart-button"
+            className="cart position-relative"
+          >
+            <i className="fas fa fa-shopping-cart fa-lg" />
+            <span className="cart-basket d-flex justify-content-center">
+              0
+            </span>
+          </Link>
+        </li>
+      </ul>
     );
   }
 }
