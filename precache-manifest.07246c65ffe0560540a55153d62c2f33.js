e094df14a44f201c309d068ce487c9327a2b9624
JonnesBezerra <JonnesBezerra@users.noreply.github.com>
2021-09-28 13:29:21 +0000

    deploy: 162a3ec331497d88f196bf415e99bf480fb38a4c

---
+++ precache-manifest.07246c65ffe0560540a55153d62c2f33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "141c8effc2a4e88986e66f8abce833d2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "71536b08c84f1b76bca5",
+    "url": "./static/css/main.ebbf067a.chunk.css"
+  },
+  {
+    "revision": "06676a18412273f87770",
+    "url": "./static/js/2.fb1d69c3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.fb1d69c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71536b08c84f1b76bca5",
+    "url": "./static/js/main.8e653b8c.chunk.js"
+  },
+  {
+    "revision": "3bad4da90a1d62b2ed0b",
+    "url": "./static/js/runtime-main.6cc6b37c.js"
+  },
+  {
+    "revision": "81c2fb57dda4b09c47c31ec70fc63652",
+    "url": "./static/media/shopping-cart-logo.81c2fb57.png"
+  },
+  {
+    "revision": "8ec60c6b97220a45f20f05a084a0be03",
+    "url": "./static/media/shopping_cart_black_24dp.8ec60c6b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e64a0c8a231ec738c1756bd0378a8e77.js"
+  "./precache-manifest.07246c65ffe0560540a55153d62c2f33.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
